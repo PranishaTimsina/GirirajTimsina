d87dcfb69d85f1ce8223bec9bfd42df82fd0853e
PranishaTimsina <timsinapranisha@gmail.com>
2026-02-17 22:20:20 +0545

    Your meaningful message here

--- contact/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('submit/', views.submit_contact_form, name='submit_contact'),
-    path('messages/', views.contact_messages_list, name='messages_list'),
-]
